346436ed81381490466c93cce810d84226807014
banba <banba@f-logic.jp>
2019-11-28 11:04:13 +0900

    no message

--- pwa4/app.js
+++ pwa4/app.js
@@ -18,7 +18,7 @@ if ('serviceWorker' in navigator) {
 var app = new Vue({
   el: '#app',
   data: {
-    ver:"1.0.7",
+    ver:"1.0.8",
     // newTask:"new task",
     // newLimit:"2019/11/27",
   },
