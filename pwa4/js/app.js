a950ac875f2b16018b598fcdbd46fa5732931418
banba <banba@f-logic.jp>
2019-11-28 15:13:30 +0900

    localStorage　実装

--- pwa4/js/app.js
+++ pwa4/js/app.js
@@ -17,14 +17,23 @@ if ('serviceWorker' in navigator) {
 var app = new Vue({
   el: '#app',
   data: {
-    ver:"1.0.9",
+    ver:"1.1.0",
+    testText:"",
     // newTask:"new task",
     // newLimit:"2019/11/27",
   },
 
   methods: {
+    saveJson:function(){
+      localStorage.setItem('save_text', this.testText);
+    },
+    deleteJson:function(){
+      // localStorage.removeItem('save_text');
+      localStorage.clear();
+    },
   },
   mounted : function(){
-    // console.log('mounted')
+    console.log('mounted')
+    this.testText = localStorage.getItem('save_text');
   }
 });
