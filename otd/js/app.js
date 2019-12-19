cc2f275c1dffe6b0bb32a46b80b850b4211edf1e
banba <banba@f-logic.jp>
2019-12-19 11:40:27 +0900

    別フォルダで追加

---
+++ otd/js/app.js
@@ -0,0 +1,205 @@
+// "use strict";
+
+//service worker 登録
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register(
+      './service_worker.js',
+      {scope: './otd'}
+    ).then(function(e) {
+        // success
+      console.log('[Service Worker:Regist]',e);
+    }).catch(function(err) {
+        // failed
+      console.log('[Service Worker:Error]',err);
+    });
+}
+
+//localStrage
+localforage.config({
+  name: 'ore_no_todo_db',
+  storeName: 'todo_json',
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    ver:"1.0.0",
+    newTab : {
+      dialog : false,
+      label:"",
+    },
+    newItem:{
+      label:"",
+      limit:new XDate().toString("yyyy-MM-dd"),
+      limitFlg:false,
+    },
+    today:new XDate().toString("yyyy-MM-dd"),
+
+    enableEditOrder:false,
+    listDrag : false,
+    todo:{
+      selectedTab:0,
+      tabs :[
+        // {
+        //   id:0,
+        //   label:"やること",
+        // },
+        // {
+        //   id:1,
+        //   label:"買うもの",
+        // },
+      ],
+      items:[
+        // {
+        //   index    : 0,
+        //   tab_id   : 0,
+        //   label    : "Todo 1",//タスク名
+        //   complete : false, //完了判定
+        //   select   : false, //削除フラグ
+        //   limit    : "2019-12-10", //完了期限
+        // },
+        // {
+        //   index    : 1,
+        //   tab_id   : 0,
+        //   label    : "Todo 2",//タスク名
+        //   complete : true, //完了判定
+        //   select   : false, //削除フラグ
+        //   limit    : "2019-12-13", //完了期限
+        // },
+        // {
+        //   index    : 2,
+        //   tab_id   : 1,
+        //   label    : "Todo 3",//タスク名
+        //   complete : false, //完了判定
+        //   select   : false, //削除フラグ
+        //   limit    : "2019-12-14", //完了期限
+        // },
+      ],
+    },
+  },
+  watch: {
+    todo: {
+      handler: function(value){
+        console.log(value);
+        this.saveJson();
+      },
+      deep: true,
+    },
+  },
+  computed: {
+    //完了したTODOの数
+    complateNum:function(){
+      var num = 0;
+      this.todo.items.forEach(function(obj){
+        //complete が trueだったら加算
+        if(obj.complete) num++;
+      });
+      return num;
+    },
+    //達成度の算出（％）
+    levelOfAchievement:function(){
+      return this.complateNum / this.todo.items.length;
+    },
+    isSelectTodo:function(){
+      return this.todo.items.some(function(obj){
+        return obj.select;
+      });
+    },
+  },
+  methods: {
+    saveJson:function(){
+      localforage.setItem('todo', this.todo)
+        .then(function(){
+          console.log("Save Success");
+        })
+        .catch(function(err){
+          console.log("Save Faild",err);
+        });
+    },
+
+    deleteJson:function(){
+      // localStorage.removeItem('testText');
+      localforage.clear();
+    },
+
+    addNewItem:function(){
+      console.log("addNewItem");
+      if(!this.newItem.label) return;
+      var items = this.todo.items;
+      items.push(
+        {
+          index    : items.length,
+          tab_id   : this.todo.selectedTab,
+          label    : this.newItem.label,
+          complate : false,
+          select   : false,
+          limit    : this.newItem.limit,
+        }
+      );
+    },//addNewItem
+
+    addNewTab:function(){
+      console.log("addNewTab");
+      if(!this.newTab.label) return;
+      var tabs = this.todo.tabs;
+      var addObj = {
+        label :this.newTab.label,
+        id    :tabs.length,
+      };
+      this.$set(tabs, tabs.length, addObj);
+    },//addNewTab
+
+    removeTab:function(key){
+      console.log("removeTab",key);
+      console.log(this.todo.tabs[key]);
+      this.$delete(this.todo.tabs, key);
+    },//removeTab
+
+    sort:function() {
+      // this.todo.items = this.todo.items.sort((a, b) => a.seq - b.seq);
+      this.list = this.list.sort((a, b) => a.order - b.order);
+    },//sort
+
+    /**
+     * [convertDate カレンダーの日付をXDate形式に変換]
+     * @param  {[string]} date ["yyyy-MM-dd"]
+     * @return {[date]}        [XDate]
+     */
+    convertDate:function(date){
+      var d_ary = date.split( '-' );
+      // console.log(d_ary1,d_ary2);
+      return new XDate(d_ary[0], Number(d_ary[1]) - 1, d_ary[2]);
+    },
+    /**
+     * [remainingDate 期限までの残りに数を算出]
+     * @param  {[string]} date [期限]
+     * @return {[number]}      [残日数]
+     */
+    remainingDate:function(date){
+      var dif = this.convertDate(this.today).diffDays( this.convertDate(date) );
+      // console.log(dif);
+      return dif;
+    },
+
+
+
+
+  },
+  mounted : function(){
+    console.log('mounted');
+
+    localforage.getItem('todo')
+      .then(function(value){
+        if(!value){
+          console.log('No Data',value);
+        }else{
+          console.log('Data Load Success',value);
+          app.todo = value;
+        }
+      })
+      .catch(function(err){
+        console.log('Data Load Faild',err);
+      });
+
+  }//mounted
+});
---
+++ otd/service_worker.js
@@ -0,0 +1,123 @@
+var CACHE_NAME  = "cache_1.0.1";
+
+var urlsToCache = [
+    "index.html",
+    "offline.html",
+    "css/app.css",
+    "js/app.js",
+    "js/vue.min.js",
+    "js/axios.min.js",
+    "js/localforage.min.js",
+    "js/xdate.js",
+    "js/sortable.min.js",
+    "js/vuedraggable.umd.min.js",
+    "images/icons/icon-72x72.png",
+];
+
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker]',event.type);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      // 指定したリソースをキャッシュへ追加
+      return cache.addAll(urlsToCache);
+      // return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+      // 1つでも失敗したらService Workerのインストールはスキップされる
+    })
+  );
+});
+
+//新しいバージョンのServiceWorkerが有効化されたとき
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker]',event.type);
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          console.log(cacheName,cacheNames);
+          // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log("[Cache Delete]",cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker]',event.request.url);
+  var online = navigator.onLine;
+
+  // ファイルパス ~/test.htmlにアクセスすると、このファイル自体は無いがServiceWorkerがResponseを作成して表示してくれる
+  if (event.request.url.indexOf('test.html') != -1) {
+    return event.respondWith(new Response('任意のURLの内容をここで自由に返却できる'));
+  }
+
+  if(online){
+    console.log("ONLINE");
+    //このパターンの処理では、Responseだけクローンすれば問題ない
+    //cloneEventRequest = event.request.clone();
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+          if (response) {
+            //オンラインでもローカルにキャッシュでリソースがあればそれを返す
+            //ここを無効にすればオンラインのときは常にオンラインリソースを取りに行き、その最新版をキャッシュにPUTする
+            return response;
+          }
+          //request streem 1
+          return fetch(event.request)
+            .then(function(response){
+              //ローカルキャッシュになかったからネットワークから落とす
+              //ネットワークから落とせてればここでリソースが返される
+
+              // Responseはストリームなのでキャッシュで使用してしまうと、ブラウザの表示で不具合が起こる(っぽい)ので、複製したものを使う
+              cloneResponse = response.clone();
+
+              if(response){
+                if(response || response.status == 200){
+                  console.log("正常にリソースを取得");
+                  caches.open(CACHE_NAME)
+                    .then(function(cache)
+                    {
+                      console.log("キャッシュへ保存");
+                      cache.put(event.request, cloneResponse)
+                      .then(function(){
+                        console.log("保存完了");
+                      });
+                    });
+                }else{
+                  return event.respondWith(new Response('200以外のエラーをハンドリングしたりできる'));
+                }
+                return response;
+              }
+            }).catch(function(error) {
+              return console.log(error);
+            });
+        })
+    );
+  }else{
+    console.log("OFFLINE");
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // キャッシュがあったのでそのレスポンスを返す
+          if (response) {
+            return response;
+          }
+          //オフラインでキャッシュもなかったパターン
+          return caches.match("offline.html")
+              .then(function(responseNodata)
+              {
+                return responseNodata;
+              });
+        }
+      )
+    );
+  }
+});
