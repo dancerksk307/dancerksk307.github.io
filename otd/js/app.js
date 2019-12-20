b8b41dcea81ce08d91ed66949ca5bb8c9ba6aed3
banba <banba@f-logic.jp>
2019-12-20 13:25:52 +0900

    no message

--- otd/js/app.js
+++ otd/js/app.js
@@ -3,8 +3,8 @@
 //service worker 登録
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(
-      '../service_worker.js',
-      {scope: './otd'}
+      './service_worker.js'
+      // ,{scope: './otd'}
     ).then(function(e) {
         // success
       console.log('[Service Worker:Regist]',e);
