9681aa4e6ea6797ca4695d2985eff5788ac24493
banba <banba@f-logic.jp>
2019-12-20 15:34:27 +0900

    TESTサンプル

---
+++ p2/sw.js
@@ -0,0 +1,31 @@
+// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
+
+// キャッシュ名とキャッシュファイルの指定
+var CACHE_NAME = 'p2-sample-caches';
+var urlsToCache = [
+	'/p2/',
+	'/p2/css/style.css',
+	'/p2/drawer.js'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
